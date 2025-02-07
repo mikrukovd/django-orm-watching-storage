8c679cd5d3d6e8e996e9e9a24f7de4a22ee79461
mikrukovd <z123456689z@gmail.com>
2025-02-07 09:12:58 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -2,7 +2,7 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
-from datacenter.models import get_duration, format_duration, is_visit_long
+from datacenter.operations_with_time import get_duration, format_duration, is_visit_long
 
 
 def passcard_info_view(request, passcode):
@@ -14,9 +14,8 @@ def passcard_info_view(request, passcode):
         enter = localtime(visit.entered_at)
         leave = localtime(visit.leaved_at)
         duration = get_duration(enter=enter, leave=leave)
-        flag = is_visit_long(duration=duration, hour=1)
-        duration = duration.total_seconds()
         duration = format_duration(duration=duration)
+        flag = is_visit_long(duration=duration, hour=1)
 
         visit_data = {
             'entered_at': enter,
